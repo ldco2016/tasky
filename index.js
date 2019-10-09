cc93a7c3f11ad3bb9a7b8c65d43b4b8a10138300
danale <danale@danales-MacBook-Pro.local>
2019-10-09 12:02:52 -0500

    Set position of bounds to be platform specific

--- index.js
+++ index.js
@@ -20,10 +20,24 @@ app.on("ready", () => {
     process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
-  tray.on("click", () => {
+  tray.on("click", (event, bounds) => {
+    // click event bounds
+    const { x, y } = bounds;
+    mainWindow.getBounds();
+
+    // Window height and width
+    const { height, width } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      const yPosition = process.platform === "darwin" ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPosition,
+        height,
+        width
+      });
       mainWindow.show();
     }
   });
