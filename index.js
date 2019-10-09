1bbbfb098d4c75c81f20e2e792e876df13f83818
danale <danale@danales-MacBook-Pro.local>
2019-10-09 11:34:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const electron = require("electron");
+
+const { app, BrowserWindow, Tray } = require("electron");
+
+let mainWindow;
+let tray;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizeable: false,
+    show: false
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  const iconName =
+    process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new Tray(iconPath);
+  tray.on("click", () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
+});
